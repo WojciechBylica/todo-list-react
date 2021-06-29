114d215e6241bc08ff943deac84d6ce41f8ea177
Wojciech Bylica <wojtekbyl@gmail.com>
2021-06-29 21:31:51 +0200

    styled-normalize added. previous normalize deleted

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Normalize } from `styled-components`;
 
 ReactDOM.render(
   <React.StrictMode>
+    <Normalize />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
