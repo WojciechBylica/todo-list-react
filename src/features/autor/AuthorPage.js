95166ac9403ed8c351c039b8fadf15e547b323fa
Wojciech Bylica <wojtekbyl@gmail.com>
2021-07-19 15:21:40 +0200

    added img to authorPage, style edit

--- src/features/autor/AuthorPage.js
+++ src/features/autor/AuthorPage.js
@@ -1,7 +1,8 @@
 import Container from "../../common/Container";
 import Section from "../../common/Section";
 import Header from "../../common/Header";
-import { Wrapper } from "./styled"
+import { Image, Wrapper } from "./styled"
+import WBTaiko from "./WBTaiko.png"
 export default () => (
     <Container>
         <Header title="O autorze" />
@@ -10,9 +11,9 @@ export default () => (
             body={
                 <>
                     <Wrapper>
+                        <Image src={WBTaiko} alt="Wojciech Bylica photo" />
                         <p>Perkusista, kompozytor, twórca o szerokich zainteresowaniach
-                            stylistycznych
-                            Absolwent UMFC w Warszawie i PSJ im.
+                            stylistycznych. Absolwent UMFC w Warszawie i PSJ im.
                             H. Majewskiego w Warszawie. Stypendysta Urbanator Days, laureat konkursu Roland V-Drums.
                         </p>
                     </Wrapper>
--- src/features/autor/styled.js
+++ src/features/autor/styled.js
@@ -4,4 +4,17 @@ export const Wrapper = styled.div`
     padding: 20px;
     background: ${({ theme }) => theme.color.white};
     text-align: justify;
+    display:flex;
+
+    @media(max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
+        flex-direction: column;
+        text-align: left;
+    }
+`;
+
+export const Image = styled.img`
+    height: 202px;
+    width: 250px;
+    border-radius: 50px;
+    margin: 0 10px;
 `;
